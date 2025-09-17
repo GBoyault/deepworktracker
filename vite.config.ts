f92ad0c54baf730381b169a3e0397a2b583cb51c
Gabriel Boyault <gabriel.boyault@chronolink.fr>
2025-09-17 13:06:37 +0200

    perf: optimize build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,22 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          motion: ["framer-motion"],
+          mui: [
+            "@emotion/react",
+            "@emotion/styled",
+            "@mui/material",
+            "@mui/x-date-pickers",
+          ],
+          moment: ["moment"],
+          router: ["react-router"],
+        },
+      },
+    },
+  },
   plugins: [react()],
 });
